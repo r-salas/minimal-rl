13b93c2f6b34364c11549ac07bb4448950886a3c
Rubén Salas <ruben.sls17@gmail.com>
2021-09-04 21:39:53 +0200

    Rename policies

--- common/policies.py
+++ common/policies.py
@@ -5,11 +5,12 @@
 #
 #
 
+import torch
 import gym.spaces
 import torch.nn as nn
 
 
-class QNetworkPolicy(nn.Module):
+class QNetworkDiscretePolicy(nn.Module):
 
     def __init__(self, observation_space: gym.spaces.Box, action_space: gym.spaces.Discrete):
         super().__init__()
@@ -22,11 +23,11 @@ class QNetworkPolicy(nn.Module):
             nn.Linear(64, action_space.n)
         )
 
-    def forward(self, x):
-        return self.fc(x)
+    def forward(self, obs):
+        return self.fc(obs)
 
 
-class ActorCriticPolicy(nn.Module):
+class ActorCriticDiscretePolicy(nn.Module):
 
     def __init__(self, observation_space: gym.spaces.Box, action_space: gym.spaces.Discrete):
         super().__init__()
@@ -41,12 +42,12 @@ class ActorCriticPolicy(nn.Module):
         self.actor = nn.Linear(64, action_space.n)
         self.critic = nn.Linear(64, 1)
 
-    def forward(self, x):
-        x = self.common(x)
+    def forward(self, obs):
+        x = self.common(obs)
         return self.critic(x), self.actor(x)
 
 
-class ActorPolicy(nn.Module):
+class ActorDiscretePolicy(nn.Module):
 
     def __init__(self, observation_space: gym.spaces.Box, action_space: gym.spaces.Discrete):
         super().__init__()
@@ -59,6 +60,73 @@ class ActorPolicy(nn.Module):
             nn.Linear(64, action_space.n)  # logits
         )
 
-    def forward(self, x):
+    def forward(self, obs):
+        x = self.fc(obs)
+        return x
+
+
+class ActorContinousPolicy(nn.Module):
+
+    def __init__(self, observation_space: gym.spaces.Box, action_space: gym.spaces.Box):
+        super().__init__()
+
+        self.fc = nn.Sequential(
+            nn.Linear(observation_space.shape[0], 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, action_space.shape[0]),
+        )
+
+    def forward(self, obs):
+        x = self.fc(obs)
+        x = torch.tanh(x)
+        return x
+
+
+class CriticDiscretePolicy(nn.Module):
+
+    def __init__(self, observation_space: gym.spaces.Box, action_space: gym.spaces.Discrete):
+        super().__init__()
+
+        self.fc = nn.Sequential(
+            nn.Linear(observation_space.shape[0] + action_space.shape[0], 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, action_space.n)  # logits
+        )
+
+    def forward(self, obs, action):
+        if obs.ndim < 2:
+            obs = obs.unsqueeze(-1)
+        if action.ndim < 2:
+            action = action.unsqueeze(-1)
+
+        x = torch.cat([obs, action], 1)
+        x = self.fc(x)
+        return x
+
+
+class CriticContinousPolicy(nn.Module):
+
+    def __init__(self, observation_space: gym.spaces.Box, action_space: gym.spaces.Box):
+        super().__init__()
+
+        self.fc = nn.Sequential(
+            nn.Linear(observation_space.shape[0] + action_space.shape[0], 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1),
+        )
+
+    def forward(self, obs: torch.Tensor, action: torch.Tensor):
+        if obs.ndim < 2:
+            obs = obs.unsqueeze(-1)
+        if action.ndim < 2:
+            action = action.unsqueeze(-1)
+
+        x = torch.cat([obs, action], 1)
         x = self.fc(x)
         return x
